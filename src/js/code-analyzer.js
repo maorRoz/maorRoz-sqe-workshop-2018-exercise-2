1bda309de5d72976fae520810d264f6d39df5e97
maorRoz <andorys@post.bgu.ac.il>
2018-11-25 17:41:33 +0200

    improvd the htm, removed line number

--- src/js/code-analyzer.js
+++ src/js/code-analyzer.js
@@ -1,7 +1,7 @@
 import * as esprima from 'esprima';
 
 const parseCode = (codeToParse) => {
-    return esprima.parseScript(codeToParse, {loc: true});
+    return esprima.parseScript(codeToParse);
 };
 
 export {parseCode};
