cbb85c2751abafc3fc7216cb8057fc89eb2f004c
maorRoz <andorys@post.bgu.ac.il>
2018-11-26 10:32:20 +0200

    improved

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,16 @@
 import $ from 'jquery';
 import {parseCode} from './code-analyzer';
 import { createElementTable, implementElementTableUI } from './controller/elementsTableController'; 
-import * as UI from '../elementsTableUI';
+import { colorMethod } from '../view';
 
 $(document).ready(function () {
     $('#codeSubmissionButton').click(() => {
         const codeToParse = $('#codePlaceholder').val();
         const parsedCode = parseCode(codeToParse);
-        $('#parsedCode').val(JSON.stringify(parsedCode, null, 2));
-        createElementTable(parsedCode);
-        implementElementTableUI(UI);
+        //$('#parsedCode').val(JSON.stringify(parsedCode, null, 2));
+        const { method, arguments } = createMethodAndArguments(parseCode);
+        const parsedMethod = symbolicSubstitution(method);
+        const evalParsedMethod = toEvalParsedMethod(parsedMethod, arguments);
+        colorMethod(evalParsedMethod);
     });
 });
